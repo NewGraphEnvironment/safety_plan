52ab80335ab185843e2a86b29a6a5a0aedc68f48
NewGraphEnvironment <al@newgraphenvironment.com>
2021-07-21 09:08:20 -0700

    copy correct index and pdf to docs

--- R/run.R
+++ R/run.R
@@ -9,8 +9,8 @@
 file.copy('C:/scripts/fish_passage_2021_permit_app_elk/R/private_info.R', 'R/private_info.R')
 
 ##move the html to the docs folder so it can be viewed online
-file.rename('application_moe.html', 'docs/index.html')
-file.rename('application_moe.pdf', 'docs/application_moe.pdf')
+file.rename('safety_plan.html', 'docs/index.html')
+file.rename('safety_plan.pdf', 'docs/safety_plan.pdf')
 
 
 
