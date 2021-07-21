daf9f0695103bcbe31b4fdb4c1ff6fc932ea2f35
NewGraphEnvironment <al@newgraphenvironment.com>
2021-07-21 10:52:12 -0700

    remove species table and program the run

--- R/run.R
+++ R/run.R
@@ -5,13 +5,17 @@
 ##change your VErsion #
 #######################################################################################
 
+
+
 ##we need to get our private data file moved over
 file.copy('C:/scripts/fish_passage_2021_permit_app_elk/R/private_info.R', 'R/private_info.R')
+
+pagedown::chrome_print('safety_plan.Rmd')
+
 
 ##move the html to the docs folder so it can be viewed online
 file.rename('safety_plan.html', 'docs/index.html')
 file.rename('safety_plan.pdf', 'docs/safety_plan.pdf')
-
 
 
 
